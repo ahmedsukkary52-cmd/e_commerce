67d6612eff5d531f4f86f2c41a5856bdebc62c0c
ahmed <ahmedsukkary52##@gmail.com>
2026-01-04 22:17:10 +0200

    Nothing has been added yet

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(const EcommerceApp());
+}
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
